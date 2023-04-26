33fd810c6aaaa147334aec680a9d298a6ddd7c46
rorisangputu <84104112+rorisangputu@users.noreply.github.com>
2023-04-26 14:55:14 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,50 +5,57 @@ class Program
     {
 
         Recipe recipe = new Recipe();
-
-        Console.WriteLine("****************************");
-        Console.WriteLine("My Cook Book Application");
-        Console.WriteLine("****************************");
-        Console.WriteLine("1. Add Recipe.");
-        Console.WriteLine("2. Display Recipe.");
-        Console.WriteLine("3. Scale Recipe");
-        Console.WriteLine("4. Reset Quantities");
-        Console.WriteLine("5. Clear Recipe");
-        Console.WriteLine("0. Exit program");
-
-        int option = int.Parse(Console.ReadLine());
-
-        switch (option)
+        while (true)
         {
-            case 1:
-                recipe.RecipeDetails();
-                break;
-                
-             case 2:
-                recipe.Display();
-                break;
+            Console.WriteLine();
+            Console.WriteLine("****************************");
+            Console.WriteLine("My Cook Book Application");
+            Console.WriteLine("****************************");
+            Console.WriteLine("1. Add Recipe.");
+            Console.WriteLine("2. Display Recipe.");
+            Console.WriteLine("3. Scale Recipe");
+            Console.WriteLine("4. Reset Quantities");
+            Console.WriteLine("5. Clear Recipe");
+            Console.WriteLine("0. Exit program");
+            Console.WriteLine();
+            int option = int.Parse(Console.ReadLine());
+            Console.WriteLine("****************************");
+            switch (option)
+            {
+                case 1:
+                    recipe.RecipeDetails();
+                    break;
 
-            case 3:
-                Console.Write("Enter Scale Factor (0.5, 2, 3): ");
-                double factor = double.Parse(Console.ReadLine());
-                recipe.Scale(factor);
-                break;
+                case 2:
+                    recipe.Display();
+                    break;
 
-            case 4:
-                recipe.resetScale();
-                break;
+                case 3:
+                    Console.Write("Enter Scale Factor (0.5, 2, 3): ");
+                    double factor = double.Parse(Console.ReadLine());
+                    recipe.Scale(factor);
+                    break;
 
-            case 5:
-                recipe.ClearDetails;
-                break;
+                case 4:
+                    recipe.resetScale();
+                    break;
 
-            case 0:
+                case 5:
+                    recipe.ClearDetails();
+                    break;
+
+                case 0:
                     Console.WriteLine("Bye");
-                return;
+                    return;
 
-            default:
-                Console.WriteLine("Invalid input. Choose numbers from menu.");
-                break; 
+                default:
+                    Console.WriteLine("Invalid input. Choose numbers from menu.");
+                    break;
+            }
         }
+
+       
     }
 }
+
+
